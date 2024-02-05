3bd6a0f012abf772ee13662568d6adc53d7eb1ec
Suprunov Anton <suprunovvanton666@gmail.com>
2024-02-06 02:41:17 +0300

    Init

---
+++ Template/Template/ViewController.swift
@@ -0,0 +1,13 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+// MARK: Вью контроллер
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .clear
+    }
+}
