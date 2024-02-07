1b3d93a851f665e8da041e23d11dbd9b3ab6df58
Suprunov Anton <suprunovvanton666@gmail.com>
2024-02-08 01:07:05 +0300

    Сделал приложение с калькулятором и "угадай число"

---
+++ Template/Template/CalculateModel.swift
@@ -0,0 +1,13 @@
+// CalculateModel.swift
+// Copyright © RoadMap. All rights reserved.
+
+import Foundation
+
+/// Варианты операций для калькулятора
+enum CalculateOperation: CaseIterable {
+    case fold
+    case subtract
+    case multiply
+    case divide
+    case cancel
+}
