8399b498429fec36e91c170da294dbf1545efa0b
Suprunov Anton <suprunovvanton666@gmail.com>
2024-02-08 15:08:30 +0300

    Сделал приложение которое меняет слово задом наперед

---
+++ Template/Template/ReversModel.swift
@@ -0,0 +1,11 @@
+// ReversModel.swift
+// Copyright © RoadMap. All rights reserved.
+
+import Foundation
+
+/// Структура с методом который переворачивает текст
+struct Revers {
+    func reversWord(_ word: String) -> String {
+        String(word.reversed())
+    }
+}
