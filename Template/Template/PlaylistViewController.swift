7d09354e69a4354b8d52833229150f26a6f9b018
Suprunov Anton <suprunovvanton666@gmail.com>
2024-02-11 23:33:12 +0300

    начал делать плеер

---
+++ Template/Template/PlaylistViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PlaylistViewController.swift
+//  Template
+//
+//  Created by MacBookPro on 11.02.2024.
+//
+
+import UIKit
+/// конроллер плейлиста
+class PlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("")
+
+    }
+
+}
