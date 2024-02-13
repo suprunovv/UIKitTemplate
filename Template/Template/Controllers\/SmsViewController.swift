d253f660354307a47935e6a090968d5ca6cb87f9
Ruslan <ruslan99992222@gmail.com>
2024-02-13 17:29:09 +0300

    Сделал все экраны

--- Template/Template/Controllers\/SmsViewController.swift
+++ Template/Template/Controllers\/SmsViewController.swift
@@ -13,6 +13,47 @@ class SmsViewController: UIViewController {
 
     // MARK: - Visual Components
 
+    private let descriptionSmsLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 35, y: 146, width: 315, height: 44))
+        label.text = "Введите кодъ изъ смс, чтобы подтвердить оплату"
+        label.font = .systemFont(ofSize: 16)
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        return label
+    }()
+
+    private let smsTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 61, y: 217, width: 262, height: 44))
+        textField.font = .boldSystemFont(ofSize: 18)
+        textField.layer.cornerRadius = 10
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor(red: 196, green: 196, blue: 196, alpha: 1).cgColor
+        textField.textAlignment = .center
+        textField.addTarget(nil, action: #selector(textDidChange), for: .editingChanged)
+        return textField
+    }()
+
+    private let sendSmsButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 109, y: 288, width: 167, height: 36))
+        button.layer.cornerRadius = 10
+        button.backgroundColor = UIColor(red: 249, green: 247, blue: 247, alpha: 1)
+        button.setTitle("Отправить снова", for: .normal)
+        button.setTitleColor(UIColor(red: 89, green: 190, blue: 190, alpha: 1), for: .normal)
+        button.addTarget(nil, action: #selector(sendSmsButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    private let confirmButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 20, y: 632, width: 345, height: 44))
+        button.layer.cornerRadius = 12
+        button.backgroundColor = UIColor(red: 89, green: 190, blue: 199, alpha: 1)
+        button.setTitle("Подтвердить", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        button.addTarget(nil, action: #selector(confirmButtonTapped), for: .touchUpInside)
+        button.isEnabled = false
+        return button
+    }()
+
     // MARK: - Public Properties
 
     // MARK: - Private Properties
@@ -39,5 +80,28 @@ class SmsViewController: UIViewController {
         backButton.tintColor = .black
         navigationItem.leftBarButtonItem = backButton
         navigationItem.title = "Кодъ из СМС"
+
+        confirmButton.alpha = 0.3
+        view.addSubview(descriptionSmsLabel)
+        view.addSubview(smsTextField)
+        view.addSubview(sendSmsButton)
+        view.addSubview(confirmButton)
+    }
+
+    @objc private func textDidChange() {
+        guard let smsText = smsTextField.text
+        else {
+            return
+        }
+        confirmButton.isEnabled = !smsText.isEmpty
+        confirmButton.alpha = confirmButton.isEnabled ? 1.0 : 0.3
+    }
+
+    @objc private func sendSmsButtonTapped() {}
+
+    @objc private func confirmButtonTapped() {
+        let nextViewController = ThankViewController()
+        nextViewController.modalPresentationStyle = .fullScreen
+        present(nextViewController, animated: false, completion: nil)
     }
 }
--- Template/Template/Controllers\/ThankViewController.swift
+++ Template/Template/Controllers\/ThankViewController.swift
@@ -5,7 +5,84 @@ import UIKit
 
 /// экран спасибо за заказ
 class ThankViewController: UIViewController {
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//    }
+    // MARK: - Types
+
+    // MARK: - Constants
+
+    // MARK: - IBOutlets
+
+    // MARK: - Visual Components
+
+    private let goodButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 20, y: 632, width: 345, height: 44))
+        button.layer.cornerRadius = 12
+        button.backgroundColor = UIColor(red: 89, green: 190, blue: 199, alpha: 1)
+        button.setTitle("Хорошо", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        button.addTarget(nil, action: #selector(goodButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    private let flowersImageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 92, y: 58, width: 200, height: 87))
+        imageView.image = UIImage(named: "Flowers")
+        return imageView
+    }()
+
+    private let thankLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 75, y: 184, width: 235, height: 128))
+        label.text = "спасибо за заказъ"
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        if let font = UIFont(name: "AmaticSC-Bold", size: 50) {
+            label.font = font
+        } else {
+            label.font = .boldSystemFont(ofSize: 50)
+        }
+
+        return label
+    }()
+
+    private let wishLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 30, y: 362, width: 315, height: 89))
+        label.text = "Разскажи о насъ другу, отправь ему промокодъ на безплатный напитокъ "
+            + "и получи скидку 10% на слѣдующій заказъ."
+        label.numberOfLines = 4
+        label.textColor = UIColor(red: 156, green: 161, blue: 168, alpha: 1)
+        label.textAlignment = .center
+        return label
+    }()
+
+    // MARK: - Public Properties
+
+    // MARK: - Private Properties
+
+    // MARK: - Initializers
+
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+
+    // MARK: - Public Methods
+
+    // MARK: - IBAction
+
+    // MARK: - Private Methods
+
+    private func setUI() {
+        view.backgroundColor = .white
+        view.addSubview(goodButton)
+        view.addSubview(flowersImageView)
+        view.addSubview(thankLabel)
+        view.addSubview(wishLabel)
+    }
+
+    @objc private func goodButtonTapped() {
+        let nextViewController = AuthorisationViewController()
+        nextViewController.modalPresentationStyle = .fullScreen
+        present(nextViewController, animated: false, completion: nil)
+    }
 }
